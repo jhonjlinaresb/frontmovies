3914853f7f34b276e706481d3d95426fe177068d
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-11-09 19:37:02 +0100

    Update Basic Home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react'
-// import logo from '../../logo.svg';
+import logo from '../../logo.svg';
 import './Home.scss'
-// import '../../App.css'
+import '../../App.css'
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
-                THIS IS THE HOME AREA
-                {/* <div className="App">
+                <div className="App">
                     <header className="App-header">
                         <img src={logo} className="App-logo" alt="logo" />
                             <p>
-                                Edit <code>src/App.js</code> and save to reload.
+                            THIS IS THE HOME AREA
                             </p>
-                            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                            Learn React
-                            </a>
                     </header>
-                </div> */}
+                </div>
             </div>
 
         )
