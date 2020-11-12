523d06e1434e5f7f16329747c0da4c4312af015e
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-11-12 17:56:51 +0100

    Fix Axios to DB for Register

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,78 +1,7 @@
-// import React from 'react'
-// import { Form, Input, Button, Checkbox } from 'antd';
-// import { UserOutlined, LockOutlined } from '@ant-design/icons';
-//import './Register.scss'
-// import { Link } from 'react-router-dom';
-
-
-// const Register = () => {
-//     const onFinish = (values) => {
-//         console.log('Received values of form: ', values);
-//     };
-
-//     return (
-//         <div className="formContainer">
-//         <Form
-//             name="normal_login"
-//             className="login-form"
-//             initialValues={{
-//                 remember: true,
-//             }}
-//             onFinish={onFinish}
-//         >
-//             <Form.Item
-//                 name="username"
-//                 rules={[
-//                     {
-//                         required: true,
-//                         message: 'Please input your Username!',
-//                     },
-//                 ]}
-//             >
-//                 <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-//             </Form.Item>
-//             <Form.Item
-//                 name="password"
-//                 rules={[
-//                     {
-//                         required: true,
-//                         message: 'Please input your Password!',
-//                     },
-//                 ]}
-//             >
-//                 <Input
-//                     prefix={<LockOutlined className="site-form-item-icon" />}
-//                     type="password"
-//                     placeholder="Password"
-//                 />
-//             </Form.Item>
-//             <Form.Item>
-//                 <Form.Item name="remember" valuePropName="checked" noStyle>
-//                     <Checkbox>Remember me</Checkbox>
-//                 </Form.Item>
-
-//                 <Link className="login-form-forgot" to="#">
-//                     Forgot password
-//         </Link>
-//             </Form.Item>
-
-//             <Form.Item>
-//                 <Button type="primary" htmlType="submit" className="login-form-button">
-//                     Log in
-//         </Button>
-//         Or <Link to="#">register now!</Link>
-//             </Form.Item>
-//         </Form>
-//         </div>
-//     );
-// };
-
-// export default Register;
-
 import React from 'react';
 import { Form, Input, Button, notification } from 'antd';
-import './Register.scss'
 import axios from 'axios';
+import './Register.scss'
 const layout = {
     labelCol: {
         span: 8,
@@ -89,7 +18,7 @@ const tailLayout = {
 };
 const Register = () => {
     const onFinish = (user) => {
-        axios.post(process.env.REACT_APP_BASE_URL+'/users/register', user)
+        axios.post(process.env.REACT_APP_BASE_URL+'/user/signup', user)
             .then(res => {
                 console.log(res.data)
                 notification.success({ message :'Usuario registrado',description:'Usuario registrado con éxito'})
