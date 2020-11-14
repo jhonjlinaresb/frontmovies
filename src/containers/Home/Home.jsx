aebecbf070c5ca764de50b006cb52d8dbae8b4b6
jhonjlinaresb <jhonjlinaresb@gmail.com>
2020-11-14 23:39:48 +0100

    Add Icon User

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,47 +1,35 @@
 
 //import logo from '../../logo.svg';
 import React, { Component } from 'react'
-import { Carousel } from 'antd'
-import { Image } from 'antd'
 import './Home.scss'
+import { Card, Image } from 'antd';
 
-const contentStyle = {
-  height: '550px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#3195FF',
-};
+const { Meta } = Card;
+
 class Home extends Component {
-    render(){
-        return (
-    <div className="home">        
-    <Carousel autoplay>
-    <div>
-    <h1 style={contentStyle}>
-    <p>Welcome to Clinic Dental</p>
-    <Image width={300} src="https://i.ibb.co/Ht0wZrK/dentalicon.png"/> 
-    </h1>
-    </div>
-    <div>
-    <h1 style={contentStyle}>
-    <p>Services Odontologics</p>
-    <Image width={400} src="https://i.ibb.co/KyP8CcL/dientes.png"/> 
-    </h1>
-    </div>
-  </Carousel>
-  </div>
-  );}
+  render(){
+      return (
+
+      <div className="home">
+       <div className="App">
+          <header className="App-header">
+          <div className="card">   
+          <a href="/login">
+          <Card
+          cover={
+          <img
+          alt="Login-user"
+          src="https://i.ibb.co/9n7G1jC/image.png"
+          />
+                }
+          >
+         </Card>
+          </a>
+          </div>
+          </header>
+            </div>
+      </div>
+      );}
 }
 
 export default Home; 
-
-{/* <div className="home">
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                            <p>
-                            THIS IS THE HOME AREA
-                            </p>
-                    </header>
-                </div> */}
