342112c065cbe2d7948f00bc191060051641ed48
Gor <g.var.design@gmail.com>
2020-11-15 19:36:02 +0100

    home content movies and movie_pk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import logo from './logo.svg';
 //import {useEffect} from 'react';
 //import {useState} from 'react';
-import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import { Switch, BrowserRouter, Route, useParams } from 'react-router-dom'
+import axios from 'axios';
 import './App.css';
 
 import Home from './containers/Home/Home'
@@ -10,6 +11,36 @@ import Register from './containers/Register/Register'
 import Login from './containers/Login/Login'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+
+const Movie = () =>{
+  const [info, setInfo] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    axios.get('https://peliculasdb.herokuapp.com/movie/id/'+id)
+    .then(res => {
+        setInfo(res.data);
+    }).catch((error)=>{
+        setInfo(false);
+    })
+}, [])
+
+  return (<div>
+    {info
+      ?<span className="movieViewer">
+        <div className="movieImg">
+          {<img className="images" src={"https://image.tmdb.org/t/p/w342" + info.poster_path} alt="poster of the movie"></img>}
+        </div>
+        <div>
+          <div className="movieGrid"><h2>{info.title}</h2></div>
+          <div className="movieGrid">{info.overview}</div>
+          <div className="movieGrid">Average vote: {info.vote_average}</div>
+        </div>  
+      </span>
+      :<span>Cargando...</span>
+    }
+    </div>);
+}
 
 function App() {
 //  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
@@ -21,12 +52,13 @@ function App() {
       <Route>
         <Header/>
         <Switch>
-        <Route path='/' exact >
-          <Home ></Home>
-        </Route>
-        <Route path='/register' component={Register} exact />
-        <Route path='/login' component={Login} exact />
-          </Switch>
+          <Route path='/' exact >
+            <Home ></Home>
+          </Route>
+          <Route path='/register' component={Register} exact />
+          <Route path='/login' component={Login} exact />
+          <Route path='/movie/:id' children={<Movie/>} exact />
+        </Switch>
         <Footer/>
         </Route>
     </BrowserRouter>
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
 //import logo from '../../logo.svg';
+import {useHistory } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Home.scss';
@@ -6,6 +7,7 @@ import './Home.scss';
 
 const Home =() =>{
     const [movie, setmovie] = useState([]);
+    const [current_page, setPage] = useState(0);
 
     useEffect(() => {
         axios.get('https://peliculasdb.herokuapp.com/movie/')
@@ -15,28 +17,78 @@ const Home =() =>{
         .catch(error => {console.log(error)});
     }, [])
     
+    const history = useHistory();
+
+    const routeChange = (item) =>{ 
+      let path = `/movie/${item.id}`; 
+      history.push(path);
+    }
+
     const Movies =()=>{
         
         return(<div>
             {movie?.map(item=>{
                 console.log(item)
-                return (<div className="grid">
-                            <div className="movieGrid">{item.title}</div>
+                return (<div className="grid" onClick={()=>{routeChange(item)}}>    
                             {<img className="images" src={"https://image.tmdb.org/t/p/w185" + item.poster_path} alt="poster of the movie"></img>}
+                            <div className="movieGrid">{item.title}</div>
                             <div className="movieGrid">Average vote: {item.vote_average}</div>
                         </div>)
             })}
         </div>);
     }
+
+    const handleScroll= event =>{
+        let max_scroll = event.target.offsetWidth;
+        let follow_button_left = document.getElementById('follow_left');
+        let follow_button_right = document.getElementById('follow_right');
+        if(event.target.scrollLeft === max_scroll){
+            follow_button_right.style.display = 'none';
+        }else{
+            follow_button_right.style.display = 'block';
+        }
+
+        if(event.target.scrollLeft === 0){
+            follow_button_left.style.display = 'none';
+        }else{
+            follow_button_left.style.display = 'block';
+        }
+    }
+
+    const updateMovie = ()=>{
+        axios.get('https://peliculasdb.herokuapp.com/movie/'+current_page)
+        .then(res => {
+            setmovie(res.data)
+        })
+        .catch(error => {console.log(error)});
+    }
+
+    const getLast = ()=>{
+        setPage(current_page-1);
+        updateMovie();
+    }
+
+    const getNext = ()=>{
+        setPage(current_page+1);
+        updateMovie();
+    }
+
     return (             
     <div>
         <div className="center">
         User has a token
+        <img src="" alt=""/>
         </div>
-        <div className="moviesContent">
+        <div className="moviesContent" onScroll={handleScroll}>
             <div className="movies">
-                    <Movies></Movies>
+                <Movies></Movies>
             </div>
+        </div>
+        <button id="follow_left" className="follow_left"> + </button>
+        <button id="follow_right" className="follow_right"> + </button>
+        <div className="page_buttons">
+            {current_page === 0 ?'':<button className="next_page" onClick={getLast}>last page</button>}
+            <button className="next_page" onClick={getNext}>next page</button>
         </div>    
     </div>
   
