81de4db59edc26a02cb491e885f3f51ae43cf610
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-11-15 19:44:38 +0100

    Update Develop App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import './App.css';
 
 import Home from './containers/Home/Home'
 import Register from './containers/Register/Register'
-<<<<<<< HEAD
-import Login from './containers/Login/Login'
-=======
 import Profile from './containers/Profile/Profile'
->>>>>>> feature/feature/UserProfile
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Login from './containers/Login/Login';
@@ -24,26 +20,11 @@ function App() {
         <Route path='/' component={Home} exact />
           <Route path='/login' component={Login} exact />
           <Route path='/register' component={Register} exact />
-<<<<<<< HEAD
-          <Route path='/login' component={Login} exact />
-=======
           <Route path='/profile' component={Profile} exact />
->>>>>>> feature/feature/UserProfile
           </Switch>
         <Footer/>
         </Route>
     </BrowserRouter>
-//   <div className="App">
-//     <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//             <p>
-//                 Edit <code>src/App.js</code> and save to reload.
-//             </p>
-//             <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//             Learn React
-//             </a>
-//     </header>
-// </div>
   );
 }
 
