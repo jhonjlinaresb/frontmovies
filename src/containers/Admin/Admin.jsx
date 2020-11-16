8fb0a8bafa1ea2a188222220e95bf4d9f64e1c00
Gor <g.var.design@gmail.com>
2020-11-16 02:32:40 +0100

    admin view finished

--- src/containers/Admin/Admin.jsx
+++ src/containers/Admin/Admin.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './Admin.scss';
 
 function Admin() {
+    const [users,setUsers] = useState([])
+    
+    useEffect(() => {
+        axios.get('https://peliculasdb.herokuapp.com/user/users')
+        .then(res => {
+            setUsers(res.data)
+        })
+        .catch(error => {console.log(error)});
+        
+    },[])
+    
     return (
-        <div className="profileContainer">
-            <section>
-                <div>
-
-                </div>
-            </section>
-
-            <section>
-
-            </section>
+        <div className="aligned">
+            {users.map(item=>{
+                return (<div className="row">
+                    {console.log(item)}
+                    <div>Name: {item.name}</div>
+                    <div>Email: {item.email}</div>
+                    {item.rented
+                    ?<div>Rented Movie: {item.rented}</div>
+                    :<div></div>
+                    }
+                </div>)
+            })}
         </div>
     )
 }
